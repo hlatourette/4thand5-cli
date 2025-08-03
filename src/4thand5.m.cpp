e5510ae894c8bce5744c5ce5e5301e0bb503d075
Hunter LaTourette <hlatourette@gmail.com>
2025-08-02 21:42:49 -0400

    refactor (feeds): update util to feeds

--- src/4thand5.m.cpp
+++ src/4thand5.m.cpp
@@ -2,14 +2,14 @@
 #include <ncurses.h>
 #include <string>
 
-#include "util.h"
+#include "feed.h"
 
 using namespace fourthandfive;
 
 int main()
 {
     initscr(); // Start curses mode
-    printw(std::to_string(meaningOfLifeTheUniverseAndEverything("")).c_str());
+    printw(std::to_string(getGameData(0)).c_str());
     refresh(); // Print it to the real screen
     getch();   // Wait for user input
     endwin();  // End curses mode
