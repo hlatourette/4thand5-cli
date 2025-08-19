63208e9f0ba0a513f2f40b6f11cc717264c5f91a
Hunter LaTourette <hlatourette@gmail.com>
2025-08-18 22:48:50 -0400

    refactor (display): temporary mvprints for clarity

--- src/4thand5.m.cpp
+++ src/4thand5.m.cpp
@@ -46,17 +46,17 @@ int main(int argc, char *argv[])
     (void)nonl();
 
     // Input + Render
-    (void)printw(std::to_string(gameState.homeTeamId).c_str());
-    (void)printw(std::to_string(gameState.awayTeamId).c_str());
-    (void)printw(std::to_string(gameState.homeTeamScore).c_str());
-    (void)printw(std::to_string(gameState.awayTeamScore).c_str());
-    (void)printw(std::to_string(gameState.homeTeamTO).c_str());
-    (void)printw(std::to_string(gameState.awayTeamTO).c_str());
-    (void)printw(std::to_string(gameState.down).c_str());
-    (void)printw(std::to_string(gameState.distance).c_str());
-    (void)printw(std::to_string(gameState.yardLine).c_str());
-    (void)printw(std::to_string(gameState.period).c_str());
-    (void)printw(std::to_string(gameState.clock.count()).c_str());
+    (void)printw(("Home " + std::to_string(gameState.homeTeamId)).c_str());
+    (void)mvprintw(1, 0, ("Away " + std::to_string(gameState.awayTeamId)).c_str());
+    (void)mvprintw(2, 0, ("Home Score " + std::to_string(gameState.homeTeamScore)).c_str());
+    (void)mvprintw(3, 0, ("Away Score " + std::to_string(gameState.awayTeamScore)).c_str());
+    (void)mvprintw(4, 0, ("Home TO " + std::to_string(gameState.homeTeamTO)).c_str());
+    (void)mvprintw(5, 0, ("Away TO " + std::to_string(gameState.awayTeamTO)).c_str());
+    (void)mvprintw(6, 0, ("Down " + std::to_string(gameState.down)).c_str());
+    (void)mvprintw(7, 0, ("Distance " + std::to_string(gameState.distance)).c_str());
+    (void)mvprintw(8, 0, ("Yard Line " + std::to_string(gameState.yardLine)).c_str());
+    (void)mvprintw(9, 0, ("Period " + std::to_string(gameState.period)).c_str());
+    (void)mvprintw(10, 0, ("Clock " + std::to_string(gameState.clock.count())).c_str());
     (void)refresh();
     int ch = getch();
     (void)printw(std::to_string(ch).c_str());
